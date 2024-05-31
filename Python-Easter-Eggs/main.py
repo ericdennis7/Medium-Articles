412c30d0fce9aab9195ec373ebdf0a67b13f77f8
ericdennis7 <ericdennis11@gmail.com>
2024-05-31 16:45:28 -0400

    Finished easter egg article

--- Python-Easter-Eggs/main.py
+++ Python-Easter-Eggs/main.py
@@ -3,7 +3,33 @@
 import this
 print(this)
 
-
 #2 - Antigravity
 import antigravity
-antigravity()
+antigravity
+
+#3 - Hello, World! from future
+from __future__ import braces
+
+#4 - Importing "Hello world!"
+import __hello__
+
+#5 - Unicode characters
+from math import pi
+
+π = pi
+r = 5
+area = π * r**2
+print(f"Area of the circle: {area}")
+
+résumé = 'knows Python'
+print('Python' in résumé)
+
+#6 - Infinity
+infinity = float('infinity')
+negative_infinity = float('-inf')
+
+hash_infinity = hash(infinity)
+hash_negative_infinity = hash(negative_infinity)
+
+print(f"Hash of positive infinity: {hash_infinity}")
+print(f"Hash of negative infinity: {hash_negative_infinity}")
